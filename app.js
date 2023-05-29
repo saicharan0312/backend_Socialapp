c67008e9815acf0e0be86b6835931d215633a977
saicharan0312 <saicharan0312@gmail.com>
2023-05-29 17:55:48 -0400

    added password,changed db name, inorder to run in any browser i added setHeaders

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 
 // working with saicharan0312@gmail.com and cluster0 collestions and places
-const url = "mongodb+srv://saicharan0312:<Password>@cluster0.wdscast.mongodb.net/places?retryWrites=true&w=majority"
+const url = "mongodb+srv://saicharan0312:Yashodha0312@cluster0.wdscast.mongodb.net/mern?retryWrites=true&w=majority"
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
 app.use('/api/places',placesRoutes);
 app.use('/api/users', usersRoutes);
