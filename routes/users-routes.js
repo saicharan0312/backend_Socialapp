af1f3e5d95d83ba500750ef3418500e330d8778b
saicharan0312 <saicharan0312@gmail.com>
2023-05-30 22:42:56 -0400

    file upload total code for front end no db connection

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 router.get('/', usersControllers.getUsers);
 
 router.post(
     '/signup', 
+    fileUpload.single('image'),
     [
         check('name').not().isEmpty(),
         check('email').normalizeEmail().isEmail(),
